3cd62f9e5ec64c62a6f78b90b14c7738ad737594
Igbalajobi Zhikrullah Oluwaranti <zalajobi@gmail.com>
2022-07-24 01:07:32 +0100

    enum with department and role statics created

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,6 @@
 import os
 from app.auth import auth_blueprint
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required
 
 
 @auth_blueprint.route('/')
@@ -7,6 +8,13 @@ def hello_world():
     return str(os.getenv('DEFAULT_PROFILE_IMG'))
 
 
-@auth_blueprint.route('/signup')
-def user_registration():
+@auth_blueprint.route('/register/provider', methods=['POST'])
+@jwt_required()
+def provider_registration():
+    id = get_jwt_identity()
     return 'password'
+
+
+@auth_blueprint.route('/login/provider')
+def login():
+    return 'HELLO'
